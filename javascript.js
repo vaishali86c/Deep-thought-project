1d71c0f5935d97ccb3792e1ec5224c52f60f6ec9
vaishali86c <vaishali86c@gmail.com>
2023-06-17 23:02:32 +0530

    Add Effective Changes In Project

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
-let btn = document.querySelector('#img2');
-let btn2 = document.querySelector('#img1');
+// // Get references to the sidebars and the toggle button
+// const sidebar1 = document.querySelector('.sidebar');
+// const sidebar2 = document.querySelector('.sidebar2');
+// const toggleButton = document.getElementById('toggleSidebarButton');
+// const toggleButton2 = document.getElementById('toggleSidebarButton2');
 
+const toggleSidebarButton1 = document.getElementById('toggleSidebarButton1'); //small
+const toggleSidebarButton2 = document.getElementById('toggleSidebarButton2'); //big
+const sidebar1 = document.querySelector('.sidebar'); //big
+const sidebar2 = document.querySelector('.sidebar2'); //small
+
+sidebar1.style.transform = 'translateX(-100%)'; // Initially hide sidebar1
+sidebar2.style.transform = 'translateX(0)'; // Initially show sidebar2
+
+toggleSidebarButton1.addEventListener('click', function() {
+  sidebar1.style.transform = 'translateX(0)';
+  sidebar2.style.transform = 'translateX(-100%)';
+});
+
+toggleSidebarButton2.addEventListener('click', function() {
+  sidebar1.style.transform = 'translateX(-100%)';
+  sidebar2.style.transform = 'translateX(0%)';
+});
+  
